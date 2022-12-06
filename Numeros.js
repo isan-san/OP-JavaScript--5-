2c10428eab2deba79d18f83b877dd79d2a32dda7
isan-san <isan.9.9f@gmail.com>
2022-12-06 17:05:34 -0500

    first commit

---
+++ Numeros.js
@@ -0,0 +1,17 @@
+let heightCM = 186;
+
+let heightM = 1.86;
+
+let weight = 82;
+
+let roundedHeightM = Math.floor(heightM);
+
+console.log("Mi altura en metros redondeada es "+ roundedHeightM);
+
+let maxValue = Number.MAX_VALUE;
+
+if (maxValue == maxValue+1) {
+    console.log("el máximo valor que se puede obtener en Javascript + 1 es igual al maximo valor que se puede obtener en JS y es: "+ maxValue+1);
+}else{
+    console.log("el máximo valor que se puede obtener en Javascript + 1 no es igual al maximo valor que se puede obtener en JS y es: "+ maxValue+1);
+}
